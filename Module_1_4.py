f325200f763741256bf7276292c3f4cd7ee64a88
Prink <121752143+Prink19@users.noreply.github.com>
2024-12-09 22:32:12 +0500

    Add files via upload

--- Module_1_4.py
+++ Module_1_4.py
@@ -1,7 +1,6 @@
 name = input('Введите назавние обучающего университета ')
 print(len(name))
 print(name.upper())
-name = 'my _ string'
 print(name.upper())
 print(name.lower())
 print(name.replace(' ' , ''))
